b3838ed5160c78c1f0e4a8670bd37c229203aea8
jdidion <github@didion.net>
2021-02-08 10:22:19 -0500

    add some new JsUtils

--- common/src/main/scala/dx/util/JsUtils.scala
+++ common/src/main/scala/dx/util/JsUtils.scala
@@ -1,7 +1,6 @@
 package dx.util
 
 import java.nio.file.Path
-
 import spray.json._
 
 import scala.collection.immutable.TreeMap
@@ -27,6 +26,25 @@ object JsUtils {
     fieldName.map(x => js.asJsObject.fields(x)).getOrElse(js)
   }
 
+  def getOptional(js: JsValue, fieldName: String): Option[JsValue] = {
+    js.asJsObject.fields.get(fieldName)
+  }
+
+  def getOptional(fields: Map[String, JsValue], fieldName: String): Option[JsValue] = {
+    fields.get(fieldName) match {
+      case None | Some(JsNull) => None
+      case some                => some
+    }
+  }
+
+  def getOptionalOrNull(js: JsValue, fieldName: String): JsValue = {
+    js.asJsObject.fields.getOrElse(fieldName, JsNull)
+  }
+
+  def getOptionalOrNull(fields: Map[String, JsValue], fieldName: String): JsValue = {
+    fields.getOrElse(fieldName, JsNull)
+  }
+
   def getFields(js: JsValue, fieldName: Option[String] = None): Map[String, JsValue] = {
     val obj = js match {
       case obj: JsObject => obj
@@ -38,11 +56,43 @@ object JsUtils {
     }
   }
 
+  def getFields(fields: Map[String, JsValue], fieldName: String): Map[String, JsValue] = {
+    fields(fieldName) match {
+      case JsObject(fields) => fields
+      case other            => throw new Exception(s"Expected JsObject, got ${other}")
+    }
+  }
+
+  def getOptionalFields(js: JsValue, fieldName: String): Option[Map[String, JsValue]] = {
+    getOptional(js, fieldName).map(getFields(_))
+  }
+
+  def getOptionalFields(fields: Map[String, JsValue],
+                        fieldName: String): Option[Map[String, JsValue]] = {
+    fields.get(fieldName).map(getFields(_))
+  }
+
   def getValues(js: JsValue, fieldName: Option[String] = None): Vector[JsValue] = {
     get(js, fieldName) match {
       case JsArray(values) => values
       case other           => throw new Exception(s"Expected JsArray, got ${other}")
     }
+  }
+
+  def getValues(fields: Map[String, JsValue], fieldName: String): Vector[JsValue] = {
+    fields.get(fieldName) match {
+      case Some(JsArray(values)) => values
+      case other                 => throw new Exception(s"Expected JsArray, got ${other}")
+    }
+  }
+
+  def getOptionalValues(js: JsValue, fieldName: String): Option[Vector[JsValue]] = {
+    getOptional(js, fieldName).map(getValues(_))
+  }
+
+  def getOptionalValues(fields: Map[String, JsValue],
+                        fieldName: String): Option[Vector[JsValue]] = {
+    fields.get(fieldName).map(getValues(_))
   }
 
   def getString(js: JsValue, fieldName: Option[String] = None): String = {
@@ -53,12 +103,44 @@ object JsUtils {
     }
   }
 
+  def getString(fields: Map[String, JsValue], fieldName: String): String = {
+    fields.get(fieldName) match {
+      case Some(JsString(value)) => value
+      case Some(JsNumber(value)) => value.toString()
+      case other                 => throw new Exception(s"Expected a string, got ${other}")
+    }
+  }
+
+  def getOptionalString(js: JsValue, fieldName: String): Option[String] = {
+    getOptional(js, fieldName).map(getString(_))
+  }
+
+  def getOptionalString(fields: Map[String, JsValue], fieldName: String): Option[String] = {
+    fields.get(fieldName).map(getString(_))
+  }
+
   def getInt(js: JsValue, fieldName: Option[String] = None): Int = {
     get(js, fieldName) match {
       case JsNumber(value) => value.toInt
       case JsString(value) => value.toInt
       case other           => throw new Exception(s"Expected a number, got ${other}")
     }
+  }
+
+  def getInt(fields: Map[String, JsValue], fieldName: String): Int = {
+    fields.get(fieldName) match {
+      case Some(JsNumber(value)) => value.toInt
+      case Some(JsString(value)) => value.toInt
+      case other                 => throw new Exception(s"Expected a number, got ${other}")
+    }
+  }
+
+  def getOptionalInt(js: JsValue, fieldName: String): Option[Int] = {
+    getOptional(js, fieldName).map(getInt(_))
+  }
+
+  def getOptionalInt(fields: Map[String, JsValue], fieldName: String): Option[Int] = {
+    fields.get(fieldName).map(getInt(_))
   }
 
   def getLong(js: JsValue, fieldName: Option[String] = None): Long = {
@@ -69,12 +151,44 @@ object JsUtils {
     }
   }
 
+  def getLong(fields: Map[String, JsValue], fieldName: String): Long = {
+    fields.get(fieldName) match {
+      case Some(JsNumber(value)) => value.toLong
+      case Some(JsString(value)) => value.toLong
+      case other                 => throw new Exception(s"Expected a number, got ${other}")
+    }
+  }
+
+  def getOptionalLong(js: JsValue, fieldName: String): Option[Long] = {
+    getOptional(js, fieldName).map(getLong(_))
+  }
+
+  def getOptionalLong(fields: Map[String, JsValue], fieldName: String): Option[Long] = {
+    fields.get(fieldName).map(getLong(_))
+  }
+
   def getDouble(js: JsValue, fieldName: Option[String] = None): Double = {
     get(js, fieldName) match {
       case JsNumber(value) => value.toDouble
       case JsString(value) => value.toDouble
       case other           => throw new Exception(s"Expected a number, got ${other}")
     }
+  }
+
+  def getDouble(fields: Map[String, JsValue], fieldName: String): Double = {
+    fields.get(fieldName) match {
+      case Some(JsNumber(value)) => value.toDouble
+      case Some(JsString(value)) => value.toDouble
+      case other                 => throw new Exception(s"Expected a number, got ${other}")
+    }
+  }
+
+  def getOptionalDouble(js: JsValue, fieldName: String): Option[Double] = {
+    getOptional(js, fieldName).map(getDouble(_))
+  }
+
+  def getOptionalDouble(fields: Map[String, JsValue], fieldName: String): Option[Double] = {
+    fields.get(fieldName).map(getDouble(_))
   }
 
   def getBoolean(js: JsValue, fieldName: Option[String] = None): Boolean = {
@@ -84,6 +198,23 @@ object JsUtils {
       case JsString("false") => false
       case other             => throw new Exception(s"Expected a boolean, got ${other}")
     }
+  }
+
+  def getBoolean(fields: Map[String, JsValue], fieldName: String): Boolean = {
+    fields.get(fieldName) match {
+      case Some(JsBoolean(value))  => value
+      case Some(JsString("true"))  => true
+      case Some(JsString("false")) => false
+      case other                   => throw new Exception(s"Expected a boolean, got ${other}")
+    }
+  }
+
+  def getOptionalBoolean(js: JsValue, fieldName: String): Option[Boolean] = {
+    getOptional(js, fieldName).map(getBoolean(_))
+  }
+
+  def getOptionalBoolean(fields: Map[String, JsValue], fieldName: String): Option[Boolean] = {
+    fields.get(fieldName).map(getBoolean(_))
   }
 
   // Make a JSON value deterministically sorted.  This is used to
