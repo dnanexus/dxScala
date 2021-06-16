284cf9b37963444ee60689933fd208a5295930be
jdidion <github@didion.net>
2021-06-16 15:43:33 -0700

    fix prettyFormat for case classes that have public fields in second parameter list

--- common/src/test/scala/dx/util/PackageTest.scala
+++ common/src/test/scala/dx/util/PackageTest.scala
@@ -3,6 +3,9 @@ package dx.util
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+case class Foo(s1: String, s2: String)(val hidden: Int = 1)
+case class Bar(foo: Foo)
+
 class PackageTest extends AnyFlatSpec with Matchers {
   it should "generate a brief exception message" in {
     try {
@@ -16,5 +19,16 @@ class PackageTest extends AnyFlatSpec with Matchers {
       case ex: Exception =>
         exceptionToString(ex, brief = true) shouldBe "exception 2\n  caused by: exception 1"
     }
+  }
+
+  it should "pretty format a case class" in {
+    val bar = Bar(Foo("value1", "value2")())
+    prettyFormat(bar, maxElementWidth = 4) shouldBe
+      """Bar(
+        |  foo = Foo(
+        |    s1 = "value1",
+        |    s2 = "value2"
+        |  )
+        |)""".stripMargin
   }
 }
