32a003d0a975b24e45f8f08705a2eebb5811bb9d
jdidion <github@didion.net>
2021-06-19 11:37:06 -0700

    improve test

--- protocols/src/test/scala/dx/util/protocols/DxTest.scala
+++ protocols/src/test/scala/dx/util/protocols/DxTest.scala
@@ -59,24 +59,44 @@ class DxTest extends AnyFlatSpec with Matchers {
     val (shallowFolders, shallowFiles) = shallowListing.partition(_.isDirectory)
     shallowFolders.size shouldBe 1
     shallowFolders.head.name shouldBe "subdir"
+    shallowFolders.head match {
+      case file: DxFolderSource => file.getParent shouldBe Some(d)
+    }
     shallowFiles.size shouldBe 1
     shallowFiles.head.name shouldBe "fileC"
+    shallowFiles.head match {
+      case file: DxFileSource => file.getParent shouldBe Some(d)
+    }
     val deepListing = d.listing(recursive = true)
     val (deepFolders, deepFiles) = deepListing.partition(_.isDirectory)
     deepFolders.size shouldBe 1
     deepFolders.head.name shouldBe "subdir"
+    deepFolders.head match {
+      case file: DxFolderSource => file.getParent shouldBe Some(d)
+    }
     deepFiles.size shouldBe 1
     deepFiles.head.name shouldBe "fileC"
+    deepFiles.head match {
+      case file: DxFileSource => file.getParent shouldBe Some(d)
+    }
     // upload file to see if change is reflected in listing
     val testFile =
       dxApi.uploadString("test", s"${dxTestProject.id}:/test_data/2/subdir/test", wait = true)
     try {
-      val shallowSubdirListing = shallowFolders.head.listing()
+      val shallowFolder = shallowFolders.head
+      val shallowSubdirListing = shallowFolder.listing()
       shallowSubdirListing.size shouldBe 2
       shallowSubdirListing.map(_.name).toSet shouldBe Set("fileA", "test")
-      val deepSubdirListing = deepFolders.head.listing()
+      shallowSubdirListing.foreach {
+        case file: DxFileSource => file.getParent shouldBe Some(shallowFolder)
+      }
+      val deepFolder = deepFolders.head
+      val deepSubdirListing = deepFolder.listing()
       deepSubdirListing.size shouldBe 1
       deepSubdirListing.head.name shouldBe "fileA"
+      deepSubdirListing.head match {
+        case file: DxFileSource => file.getParent shouldBe Some(deepFolder)
+      }
     } finally {
       dxTestProject.removeObjects(Vector(testFile))
     }
