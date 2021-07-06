fe6de34bb0af47ba958bb0cfc77e19f06bdcc6d7
jdidion <github@didion.net>
2021-07-06 11:54:38 -0700

    add conveinence constructor

--- api/src/main/scala/dx/api/DxConstraint.scala
+++ api/src/main/scala/dx/api/DxConstraint.scala
@@ -12,16 +12,26 @@ object DxConstraintOper {
 trait DxConstraint {
   def toJson: JsValue
 }
+
 case class DxConstraintString(value: String) extends DxConstraint {
   override def toJson: JsValue = JsString(value)
 }
+
 case class DxConstraintKV(key: String, value: String) extends DxConstraint {
   override def toJson: JsValue = JsObject(key -> JsString(value))
 }
+
 case class DxConstraintBool(oper: DxConstraintOper, constraints: DxConstraintArray)
     extends DxConstraint {
   override def toJson: JsValue = JsObject(oper.name -> constraints.toJson)
 }
+
 case class DxConstraintArray(constraints: Vector[DxConstraint]) extends DxConstraint {
   override def toJson: JsValue = JsArray(constraints.map(_.toJson))
+}
+
+object DxConstraintArray {
+  def apply(constraints: DxConstraint*): DxConstraintArray = {
+    DxConstraintArray(constraints.toVector)
+  }
 }
