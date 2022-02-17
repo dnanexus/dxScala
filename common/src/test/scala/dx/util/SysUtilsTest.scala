134f9fc0acc845ed82c8018120484c8076407b3f
Gvaihir <pcf11differentiation@gmail.com>
2022-02-17 10:44:58 -0800

    APPS-1006 Update include task error trace (fix) SysUtilsTest.scala - reformatted

--- common/src/test/scala/dx/util/SysUtilsTest.scala
+++ common/src/test/scala/dx/util/SysUtilsTest.scala
@@ -3,24 +3,30 @@ package dx.util
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class SysUtilsTest extends AnyFlatSpec with Matchers{
+class SysUtilsTest extends AnyFlatSpec with Matchers {
   "SysUtils" should "return code 127, stdout and stderr messages" in {
     val cmd = "(echo 'Hi'; nonexisting-command)"
     val (rc, stdout, stderr) = SysUtils.runCommand(
-      command = cmd, exceptionOnFailure = false, stdoutMode = StdMode.Capture, stderrMode = StdMode.Capture
+        command = cmd,
+        exceptionOnFailure = false,
+        stdoutMode = StdMode.Capture,
+        stderrMode = StdMode.Capture
     )
-    rc shouldBe(127)
-    stdout shouldBe(Some("Hi\n"))
-    stderr shouldBe(Some("/bin/sh: nonexisting-command: command not found\n"))
+    rc shouldBe (127)
+    stdout shouldBe (Some("Hi\n"))
+    stderr shouldBe (Some("/bin/sh: nonexisting-command: command not found\n"))
   }
 
   it should "return code 127, capture and forward stdout and stderr messages to the console" in {
     val cmd = "echo 'Hi'; nonexisting-command"
     val (rc, stdout, stderr) = SysUtils.runCommand(
-      command = cmd, exceptionOnFailure = false, stdoutMode = StdMode.Forward, stderrMode = StdMode.Forward
+        command = cmd,
+        exceptionOnFailure = false,
+        stdoutMode = StdMode.Forward,
+        stderrMode = StdMode.Forward
     )
-    rc shouldBe(127)
-    stdout shouldBe(None)
-    stderr shouldBe(None)
+    rc shouldBe (127)
+    stdout shouldBe (None)
+    stderr shouldBe (None)
   }
 }
